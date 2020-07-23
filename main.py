3f04be1590622f22b3ebbf794526e095e7e47bbe
AlphaHorologii <46262090+AlphaHorologii@users.noreply.github.com>
2020-07-24 06:56:52 +0800

    。

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import matplotlib
 import pandas
+import numpy as np
 import json
-import sklearn
+from sklearn.decomposition import PCA
 
 fp = open("test_data.json", "r", encoding="UTF-8")
 data = json.load(fp)
@@ -17,20 +18,30 @@ for user in users:
         if (len(question.get("upload_records")) > 1) and (question.get("upload_records")[0].get("score") == 100.0):
             firstACCount = 1
 
+        scoreSum = sum([x.get("score") for x in question.get("upload_records")])
+
         if question.get("case_id") in questions:
             questions[question.get("case_id")][0] += 1
             questions[question.get("case_id")][1] += len(question.get("upload_records"))
             questions[question.get("case_id")][2] += question.get("final_score")
-            questions[question.get("case_id")][3] += ACCount
-            questions[question.get("case_id")][4] += firstACCount
+            questions[question.get("case_id")][3] += scoreSum
+            questions[question.get("case_id")][4] += ACCount
+            questions[question.get("case_id")][5] += firstACCount
         else:
             questions[question.get("case_id")] = [
                 1,
                 len(question.get("upload_records")),
                 question.get("final_score"),
+                scoreSum,
                 ACCount,
                 firstACCount
             ]
 
-print(questions)
+for key in questions:
+    questions[key][2] = questions[key][2] / questions[key][0]
+    questions[key][3] = questions[key][3] / questions[key][1]
+    questions[key][4] = questions[key][4] / questions[key][0]
+    questions[key][5] = questions[key][5] / questions[key][0]
+    print(questions[key])
+
 print(len(data))
