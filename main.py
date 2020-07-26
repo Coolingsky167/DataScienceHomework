8e5dcb44268d678a6b0f4c7921295acd778438e4
AlphaHorologii <46262090+AlphaHorologii@users.noreply.github.com>
2020-07-26 18:15:17 +0800

    .

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 
-import Download
+from download import Download
 import getQuestions
 
 fp = open("test_data.json", "r", encoding="UTF-8")
@@ -12,18 +12,20 @@ data = json.load(fp)
 questions = getQuestions.get_questions(data)
 
 keys = questions.keys()
-X = pd.DataFrame([x for x in [questions.get(key) for key in keys]])
+X = pd.DataFrame([x for x in
+                  [questions.get(key) for key in keys]])
 
 pca = PCA(n_components=0.95)
 X_reduction = pca.fit_transform(X)
+print(pca.explained_variance_ratio_)
 for x in X_reduction:
-    print(x)
+    print(x[0] * pca.explained_variance_ratio_[0] + x[1] * pca.explained_variance_ratio_[1])
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 ax.set_xlabel('Principal Component 1', fontsize=15)
 ax.set_ylabel('Principal Component 2', fontsize=15)
 ax.set_title('2 Component PCA', fontsize=20)
-ax.scatter([x[0] for x in X_reduction], [x[1] for x in X_reduction], alpha=0.8)
+ax.scatter([x[0] for x in X_reduction], [x[1] for x in X_reduction])
 ax.grid()
 plt.show()
